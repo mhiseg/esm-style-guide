c4169b83b7aa46bf8ea5aaece118b812d51757e1
casimirMorency <morencycasimir@gmail.com>
2022-05-20 16:05:04 -0400

    added card for mhiseg main content in death management

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "./set-public-path.js";
+
+import "./load-css.js";
+import "@openmrs/style-referenceapplication/lib/referenceapplication.css";
+import "./variables.css";
+import "./custom.css";
+import "./overrides/death-management.css"
+
+
+function setupOpenMRS() {
+    return {
+      lifecycle: () => Promise.resolve(),
+      activate: () => false,
+    };
+  }
+  
+  export { setupOpenMRS };
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    libraryTarget: "system",
+    filename: "openmrs-esm-refapp-styleguide.js",
+    chunkFilename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    jsonpFunction: "webpackJsonp_openmrs_esm_refapp_styleguide"
+  },
+  mode: "production",
+  devtool: "sourcemap",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          "css-loader",
+          "postcss-loader"
+        ]
+      },
+      {
+        test: /\.(woff2|woff|ttf|eot|svg|png|gif)?$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(html)$/,
+        use: ["raw-loader"]
+      }
+    ]
+  },
+  devServer: {
+    disableHostCheck: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "openmrs-esm-refapp-styleguide.css" })
+  ]
+};
